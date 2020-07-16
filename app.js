35e41fd18eada97ab70d7081f502f4296d88b9d8
Durell <mdmwakulo@gmail.com>
2020-07-16 18:05:37 +0000

    ohhh...

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes      = require("./routes/index")
 
-mongoose.connect("process.env.DATABASEURL");
+mongoose.connect(process.env.DATABASEURL);
 // mongoose.connect("mongodb+srv://devsprout:durell1234@cluster0.xycrh.mongodb.net/<dbname>?retryWrites=true&w=majority");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
